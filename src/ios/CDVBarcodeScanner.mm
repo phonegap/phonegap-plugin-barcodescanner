236630da25e4e8dc5c5fe83b43611bfa6ba88c9d
jonatas <jonatascrossi@gmail.com>
2014-07-22 20:58:34 -0300

    fix auto focus on ios 7

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -316,7 +316,27 @@ parentViewController:(UIViewController*)parentViewController
     [self performSelector:@selector(barcodeScanCancelled) withObject:nil afterDelay:0];
     [self performSelector:@selector(scanBarcode) withObject:nil afterDelay:0.1];
 }
+//--------------------------------------------------------------------------
+- (void) autoFocus{
+    NSError* error = nil;
+    NSArray *devices = [AVCaptureDevice devices];
+    
+    for (AVCaptureDevice *device in devices) {
+        if ([device position] == AVCaptureDevicePositionBack) {
+            [device lockForConfiguration:&error];
+            if ([device isFocusModeSupported:AVCaptureFocusModeContinuousAutoFocus])
+            {
+                CGPoint autofocusPoint = CGPointMake(0.5f, 0.5f);
+                [device setFocusPointOfInterest:autofocusPoint];
+                [device setFocusMode:AVCaptureFocusModeContinuousAutoFocus];
+            }
+            
+            [device unlockForConfiguration];
+        }
+    }
 
+    [NSThread sleepForTimeInterval:1.0f];
+}
 //--------------------------------------------------------------------------
 - (NSString*)setUpCaptureSession {
     NSError* error = nil;
@@ -422,7 +442,7 @@ parentViewController:(UIViewController*)parentViewController
     uint8_t* imageBytes;
     
     //        NSTimeInterval timeStart = [NSDate timeIntervalSinceReferenceDate];
-    
+    [self autoFocus];
     try {
         DecodeHints decodeHints;
         decodeHints.addFormat(BarcodeFormat_QR_CODE);
