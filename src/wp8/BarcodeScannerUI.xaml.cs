22da50e390c2a1e690ca4b10a15a6f757699c827
Sébastien Ollivier <sebastien.ollivier@gmail.com>
2015-04-10 16:07:11 +0200

    Fix WP8
    Force the focus of the camera
    Fix the plugin when the back button is pressed
    Fix the plugin when the app is tombstoned

--- src/wp8/BarcodeScannerUI.xaml.cs
+++ src/wp8/BarcodeScannerUI.xaml.cs
@@ -1,12 +1,17 @@
-﻿﻿/*
- * Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
+﻿/*
+* Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WPCordovaClassLib.Cordova.Commands
 {
@@ -41,28 +46,25 @@ namespace WPCordovaClassLib.Cordova.Commands
         /// </summary>
         private PhotoCamera camera;
 
+        private CancellationTokenSource cancellationTokenSource;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="BarcodeScannerUI"/> class.
         /// This implementation not use camera autofocus.
         /// </summary>
         public BarcodeScannerUI()
         {
-            this.InitializeComponent();
-
-            // Instantiate objects and start camera preview
-            this.camera = new PhotoCamera();
-            this.reader = new BarcodeReader {Options = {TryHarder = true}};
-            this.CameraBrush.SetSource(this.camera);
-
-            // Bind events
-            this.camera.Initialized += this.CameraInitialized;
-            this.reader.ResultFound += this.ReaderResultFound;
+            InitializeComponent();
         }
 
         /// <summary>
         /// Occurs when barcode scan is [completed].
         /// </summary>
         public event EventHandler<BarcodeScannerTask.ScanResult> Completed;
+
+        private bool isInit = false;
+
+        private bool shouldDispose = false;
 
         /// <summary>
         /// Called when a page is no longer the active page in a frame.
@@ -71,10 +73,36 @@ namespace WPCordovaClassLib.Cordova.Commands
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             // If result is null, user is cancelled scan operation
+
             this.result = this.result ?? new BarcodeScannerTask.ScanResult(TaskResult.Cancel);
-            this.Completed(this, this.result);
-            this.CleanUp();
+            BackKeyPress -= BarcodeScannerUI_BackKeyPress;
+            Completed(this, this.result);
+            CleanUp();
             base.OnNavigatedFrom(e);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            BackKeyPress += BarcodeScannerUI_BackKeyPress;
+            cancellationTokenSource = new CancellationTokenSource();
+
+            // Instantiate objects and start camera preview
+            this.camera = new PhotoCamera();
+            this.reader = new BarcodeReader { Options = { TryHarder = true } };
+
+            CameraBrush.SetSource(this.camera);
+
+            // Bind events
+            this.camera.Initialized += CameraInitialized;
+            this.reader.ResultFound += ReaderResultFound;
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void BarcodeScannerUI_BackKeyPress(object sender, CancelEventArgs e)
+        {
+            this.result = new BarcodeScannerTask.ScanResult(TaskResult.Cancel);
+            NavigationService.GoBack();
         }
 
         /// <summary>
@@ -84,24 +112,61 @@ namespace WPCordovaClassLib.Cordova.Commands
         /// <param name="e">The <see cref="CameraOperationCompletedEventArgs"/> instance containing the event data.</param>
         private void CameraInitialized(object sender, CameraOperationCompletedEventArgs e)
         {
+            this.isInit = true;
+
+            if (this.shouldDispose)
+            {
+                CleanUp();
+                return;
+            }
+
             if (e.Succeeded)
             {
+
+
+                if (this.camera.IsFlashModeSupported(FlashMode.Off))
+                {
+                    this.camera.FlashMode = FlashMode.Off;
+                }
+
                 // Start scan process in separate thread
-                Deployment.Current.Dispatcher.BeginInvoke(
-                    () =>
+                Task.Factory.StartNew(async
+                    (_) =>
+                {
+                    while (this.result == null)
+                    {
+
+                        var waitTask = Task.Delay(1500);
+
+                        if (this.camera.IsFocusSupported)
                         {
-                            while (result == null)
-                            {
-                                var cameraBuffer = new WriteableBitmap(
-                                    (int)camera.PreviewResolution.Width,
-                                    (int)camera.PreviewResolution.Height);
+                            this.camera.Focus();
+                        }
 
-                                camera.GetPreviewBufferArgb32(cameraBuffer.Pixels);
-                                cameraBuffer.Invalidate();
+                        await waitTask;
 
-                                reader.Decode(cameraBuffer);
-                            }
-                        });
+                        int width = (int)this.camera.PreviewResolution.Width;
+                        int height = (int)this.camera.PreviewResolution.Height;
+                        int size = width * height;
+                        var cameraBuffer = new int[size];
+                        this.camera.GetPreviewBufferArgb32(cameraBuffer);
+
+                        var converted = new byte[size * sizeof(int)];
+                        Buffer.BlockCopy(cameraBuffer, 0, converted, 0, converted.Length);
+
+                        try
+                        {
+                            this.reader.Decode(converted, width, height, RGBLuminanceSource.BitmapFormat.BGRA32);
+                        }
+                        catch (Exception)
+                        {
+
+
+                        }
+
+
+                    }
+                }, TaskCreationOptions.LongRunning | TaskCreationOptions.DenyChildAttach, CancellationToken.None);
             }
             else
             {
@@ -118,26 +183,39 @@ namespace WPCordovaClassLib.Cordova.Commands
         {
             VibrateController.Default.Start(TimeSpan.FromMilliseconds(100));
             this.result = new BarcodeScannerTask.ScanResult(TaskResult.OK) { Barcode = obj };
-            NavigationService.GoBack();
+            Deployment.Current.Dispatcher.BeginInvoke(() => NavigationService.GoBack());
         }
 
         /// <summary>
         /// Cleans up resources and removes unnecessary callbacks.
         /// </summary>
-        private void CleanUp()
+        public void CleanUp()
         {
             if (this.camera != null)
             {
-                this.camera.Initialized -= this.CameraInitialized;
-                this.camera.Dispose();
+                if (this.isInit)
+                {
+                    this.camera.Initialized -= CameraInitialized;
+                    this.camera.Dispose();
+                }
+                else
+                {
+                    this.shouldDispose = true;
+                }
+
                 this.camera = null;
             }
 
             if (this.reader != null)
             {
-                this.reader.ResultFound -= this.ReaderResultFound;
+                this.reader.ResultFound -= ReaderResultFound;
                 this.reader = null;
             }
+        }
+
+        private void BarcodeScannerUI_OnBackKeyPress(object sender, CancelEventArgs e)
+        {
+            NavigationService.GoBack();
         }
     }
 }
