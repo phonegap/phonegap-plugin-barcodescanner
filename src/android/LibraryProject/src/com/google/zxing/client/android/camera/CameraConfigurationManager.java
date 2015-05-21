7eb79f264903765bbc684a0ec8120de22b1914bc
Marcin Golonka <meron1122@gmail.com>
2015-05-21 20:29:24 +0200

    Update CameraConfigurationManager.java

--- src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraConfigurationManager.java
+++ src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraConfigurationManager.java
@@ -67,7 +67,7 @@ final class CameraConfigurationManager {
     int height = display.getHeight();
     // We're landscape-only, and have apparently seen issues with display thinking it's portrait 
     // when waking from sleep. If it's not landscape, assume it's mistaken and reverse them:
-    if (width < height) {
+    if    (width > height) {
       Log.i(TAG, "Display reports portrait orientation; assuming this is incorrect");
       int temp = width;
       width = height;
