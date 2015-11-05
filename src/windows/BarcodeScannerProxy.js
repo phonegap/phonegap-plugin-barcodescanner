806c5167b2d7dde362f00e839fc86f80bbb5d2a7
convey-gilbert <gilbert@convey.de>
2015-11-05 12:35:42 +0100

    Use of back camera on windows
    Enumerate available video devices and take first back panel video device
    as media source now

--- src/windows/BarcodeScannerProxy.js
+++ src/windows/BarcodeScannerProxy.js
@@ -52,62 +52,87 @@ module.exports = {
             var captureSettings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
             captureSettings.streamingCaptureMode = Windows.Media.Capture.StreamingCaptureMode.video;
             captureSettings.photoCaptureSource = Windows.Media.Capture.PhotoCaptureSource.videoPreview;
+            
+            // Enumerate cameras and add find first back camera
+            var cameraId = null;
+            var deviceInfo = Windows.Devices.Enumeration.DeviceInformation;
+            if (deviceInfo) {
+                deviceInfo.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture).done(function (cameras) {
+                    if (cameras && cameras.length > 0) {
+                        cameras.forEach(function (camera) {
+                            if (camera && !cameraId) {
+                                // Make use of the camera's location if it is available to the description
+                                var camLocation = camera.enclosureLocation;
+                                if (camLocation && camLocation.panel === Windows.Devices.Enumeration.Panel.back) {
+                                    cameraId = camera.id;
+                                }
+                            }
+                        });
+                    }
+                    if (cameraId) {
+                        captureSettings.videoDeviceId = cameraId;
+                    }
 
-            capture.initializeAsync(captureSettings).done(function () {
+                    capture.initializeAsync(captureSettings).done(function () {
 
-                //trying to set focus mode
-                var controller = capture.videoDeviceController;
+                        //trying to set focus mode
+                        var controller = capture.videoDeviceController;
 
-                if (controller.focusControl && controller.focusControl.supported) {
-                    if (controller.focusControl.configure) {
-                        var focusConfig = new Windows.Media.Devices.FocusSettings();
-                        focusConfig.autoFocusRange = Windows.Media.Devices.AutoFocusRange.macro;
+                        if (controller.focusControl && controller.focusControl.supported) {
+                            if (controller.focusControl.configure) {
+                                var focusConfig = new Windows.Media.Devices.FocusSettings();
+                                focusConfig.autoFocusRange = Windows.Media.Devices.AutoFocusRange.macro;
 
-                        var supportContinuousFocus = controller.focusControl.supportedFocusModes.indexOf(Windows.Media.Devices.FocusMode.continuous).returnValue;
-                        var supportAutoFocus = controller.focusControl.supportedFocusModes.indexOf(Windows.Media.Devices.FocusMode.auto).returnValue;
+                                var supportContinuousFocus = controller.focusControl.supportedFocusModes.indexOf(Windows.Media.Devices.FocusMode.continuous).returnValue;
+                                var supportAutoFocus = controller.focusControl.supportedFocusModes.indexOf(Windows.Media.Devices.FocusMode.auto).returnValue;
 
-                        if (supportContinuousFocus) {
-                            focusConfig.mode = Windows.Media.Devices.FocusMode.continuous;
-                        } else if (supportAutoFocus) {                        
-                            focusConfig.mode = Windows.Media.Devices.FocusMode.auto;
+                                if (supportContinuousFocus) {
+                                    focusConfig.mode = Windows.Media.Devices.FocusMode.continuous;
+                                } else if (supportAutoFocus) {
+                                    focusConfig.mode = Windows.Media.Devices.FocusMode.auto;
+                                }
+
+                                controller.focusControl.configure(focusConfig);
+                                controller.focusControl.focusAsync();
+                            }
                         }
 
-                        controller.focusControl.configure(focusConfig);
-                        controller.focusControl.focusAsync();
-                    }
-                }
+                        var deviceProps = controller.getAvailableMediaStreamProperties(Windows.Media.Capture.MediaStreamType.videoRecord);
 
-                var deviceProps = controller.getAvailableMediaStreamProperties(Windows.Media.Capture.MediaStreamType.videoRecord);
+                        deviceProps = Array.prototype.slice.call(deviceProps);
+                        deviceProps = deviceProps.filter(function (prop) {
+                            // filter out streams with "unknown" subtype - causes errors on some devices
+                            return prop.subtype !== "Unknown";
+                        }).sort(function (propA, propB) {
+                            // sort properties by resolution
+                            return propB.width - propA.width;
+                        });
 
-                deviceProps = Array.prototype.slice.call(deviceProps);
-                deviceProps = deviceProps.filter(function (prop) {
-                    // filter out streams with "unknown" subtype - causes errors on some devices
-                    return prop.subtype !== "Unknown";
-                }).sort(function (propA, propB) {
-                    // sort properties by resolution
-                    return propB.width - propA.width;
+                        var maxResProps = deviceProps[0];
+
+                        controller.setMediaStreamPropertiesAsync(Windows.Media.Capture.MediaStreamType.videoRecord, maxResProps).done(function () {
+                            // handle portrait orientation
+                            if (Windows.Graphics.Display.DisplayProperties.nativeOrientation == Windows.Graphics.Display.DisplayOrientations.portrait) {
+                                capture.setPreviewRotation(Windows.Media.Capture.VideoRotation.clockwise90Degrees);
+                                capturePreview.msZoom = true;
+                            }
+
+                            capturePreview.src = URL.createObjectURL(capture);
+                            capturePreview.onloadeddata = function () {
+                                updatePreviewForRotation();
+                            };
+                            capturePreview.play();
+
+                            // Insert preview frame and controls into page
+                            document.body.appendChild(capturePreview);
+                            document.body.appendChild(capturePreviewAlignmentMark);
+                            document.body.appendChild(captureCancelButton);
+
+                            startBarcodeSearch(maxResProps.width, maxResProps.height);
+                        });
+                    });
                 });
-
-                var maxResProps = deviceProps[0];
-
-                controller.setMediaStreamPropertiesAsync(Windows.Media.Capture.MediaStreamType.videoRecord, maxResProps).done(function () {
-                    // handle portrait orientation
-                    if (Windows.Graphics.Display.DisplayProperties.nativeOrientation == Windows.Graphics.Display.DisplayOrientations.portrait) {
-                        capture.setPreviewRotation(Windows.Media.Capture.VideoRotation.clockwise90Degrees);
-                        capturePreview.msZoom = true;
-                    }
-
-                    capturePreview.src = URL.createObjectURL(capture);
-                    capturePreview.play();
-
-                    // Insert preview frame and controls into page
-                    document.body.appendChild(capturePreview);
-                    document.body.appendChild(capturePreviewAlignmentMark);
-                    document.body.appendChild(captureCancelButton);
-
-                    startBarcodeSearch(maxResProps.width, maxResProps.height);
-                });
-            });
+            }
         }
 
         /**
