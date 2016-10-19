73cb5b08f85c761dd45f2ef50cc8f4142f7951f4
jariztia <jariztia@kunder.cl>
2016-10-19 10:28:45 -0300

    Import bundle for onRestoreStateForActivityResult method

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -15,6 +15,7 @@ import org.json.JSONObject;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.content.pm.PackageManager;
 
