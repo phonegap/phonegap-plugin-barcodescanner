ca72095ec71e5173131f0475f94d5b6e100e93b5
Christoph Reinbothe <LSBOSS@users.noreply.github.com>
2017-09-05 18:46:28 +0200

    Fulfilled pull request requirements

--- src/windows/BarcodeScannerProxy.js
+++ src/windows/BarcodeScannerProxy.js
@@ -158,12 +158,12 @@ BarcodeReader.prototype.init = function (capture, width, height) {
     this._zxingReader = new ZXing.BarcodeReader();
     this._zxingReader.tryHarder = true;
 
-    const formatsList = BarcodeReader.scanCallArgs.args.length > 0 && BarcodeReader.scanCallArgs.args[0].formats;	
+    var formatsList = BarcodeReader.scanCallArgs.args.length > 0 && BarcodeReader.scanCallArgs.args[0].formats;	
 	if (formatsList) {		
         var possibleFormats = formatsList
             .split(",")
             .map(format => {
-                for (let index in BARCODE_FORMAT) {
+                for (var index in BARCODE_FORMAT) {
                     if (BARCODE_FORMAT[index] === format) {                        
                         return index;
                     }
