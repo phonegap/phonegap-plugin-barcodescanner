a078531638a28e5d7068ba72d4d6789d1bebc9d4
fokazaki <>
2020-02-08 09:16:52 +0900

    test

--- www/barcodescanner.js
+++ www/barcodescanner.js
@@ -105,11 +105,11 @@ BarcodeScanner.prototype.scan = function (successCallback, errorCallback, config
             }
 
             if (scanInProgress) {
-                //errorCallback('Scan is already in progress');
-                //return;
+                errorCallback('Scan is already in progress');
+                return;
             }
 
-            //scanInProgress = true;
+            scanInProgress = true;
 
             exec(
                 function(result) {
