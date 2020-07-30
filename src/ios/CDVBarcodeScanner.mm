e7e4ba1af752b19cc0f1b653913c9a0312ab6216
Srikanth Myadam <srikanth.myadam@accenture.com>
2020-07-30 14:01:29 +0530

    Set the presentation to full screen explicity from iOS 13.0

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -358,6 +358,9 @@ parentViewController:(UIViewController*)parentViewController
 
 //--------------------------------------------------------------------------
 - (void)openDialog {
+    if (@available(iOS 13.0, *)) {
+        [self.viewController setModalPresentationStyle:UIModalPresentationFullScreen];
+    }
     [self.parentViewController
      presentViewController:self.viewController
      animated:self.isTransitionAnimated completion:nil
